a5f3e1754e6f5bb5ec6e7d534a15c3be0afffba6
Vikas R. Desale <vikas.desaleu@gmail.com>
2016-12-15 10:50:14 +0530

    Update Urls.java

--- app/src/main/java/com/vikas/elibrary/json/Urls.java
+++ app/src/main/java/com/vikas/elibrary/json/Urls.java
@@ -4,7 +4,7 @@ package com.vikas.elibrary.json;
  * Created by Dell on 12/19/2015.
  */
 public class Urls {
-    public static final String BOOK_URL_1_12="http://www.noticeb.net46.net/index1.php";
+    public static final String BOOK_URL_1_12="URL";
 
 
 }
